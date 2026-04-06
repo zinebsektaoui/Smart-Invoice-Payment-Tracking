e2b4e97294e0d47e47808bd18b5fff6e1c7ae00d
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-04-06 17:16:56 +0100

    L'ajout des diagrammes de use case et de classe

---
+++ Models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true,
+        trim : true
+    },email : {
+        type : String,
+        required : true,
+        unique : true
+    },password : {
+        type : String,
+        required : true,
+    },role : {
+        type : String,
+        enum : ["Client", "admin"],
+        default : "Client"
+    }
+})
+const User = mongoose.model("User", userSchema)
+module.exports = User
