e592e79058d77d7be04f93c135d03005370c97eb
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-04-07 12:29:45 +0100

    vérification du token

---
+++ Middlewares/authMiddleware.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken")
+
+const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers.authorization;// Jib token men header
+    if (!authHeader) {
+        return res.status(401).json({ message: "Access denied Token manquant." });
+    }
+    const token = authHeader.split(" ")[1]; 
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: "Token invalide" });
+    }
+};
+
+module.exports = { authMiddleware }
