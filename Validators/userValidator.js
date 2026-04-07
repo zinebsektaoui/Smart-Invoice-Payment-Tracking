2c19e99a8c3cfca92a54bab4c3f7bddb889c2596
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-04-07 17:55:23 +0100

    validation détaillé

--- Validators/userValidator.js
+++ Validators/userValidator.js
@@ -1,10 +1,22 @@
-const joi = require("joi")
+const Joi = require("joi")
 
-const createUser = joi.object({
-  name: joi.string().required().min(2),
-  email:joi.string().email().required(),
-  password:joi.string().required().min(8),
-  confirmPassword:joi.ref("password")
+const userValidator = Joi.object({
+    name: Joi.string().min(2).required().messages({
+        'string.min': 'Le nom doit contenir au moins 3 caractères',
+        'string.max': 'Le nom ne peut pas dépasser 20 caractères',
+        'any.required': 'Le nom est requis'
+    }),
+    email: Joi.string().email().required().messages({
+        'string.email': 'Format d\'email invalide',
+        'any.required': 'L\'email est requis'
+    }),
+    password: Joi.string().min(8).required().messages({
+        'string.min': 'Le mot de passe doit contenir au moins 6 caractères',
+        'any.required': 'Le mot de passe est requis'
+    }),
+  confirmPassword:Joi.ref("password"),
+    role: Joi.string().valid("Admin", "Client").default('Client'),
+
 });
 
-module.exports = {createUser}
+module.exports = userValidator
