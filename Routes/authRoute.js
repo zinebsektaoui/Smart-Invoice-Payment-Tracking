25da50934c04f9980773608911dcc97974d98a8d
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-04-07 17:53:57 +0100

    l'ajout du profile route

--- Routes/authRoute.js
+++ Routes/authRoute.js
@@ -1,10 +1,13 @@
 const express = require("express")
-const { registerController, loginController } = require("../Controllers/authController");
-const { validateMiddleware } = require("../Middlewares/validateMiddleware")
+const { registerController, loginController, myProfileController } = require("../Controllers/authController");
+const validateMiddleware = require("../Middlewares/validateMiddleware")
+const userValidator = require("../Validators/userValidator")
+const {authMiddleware} = require("../Middlewares/authMiddleware")
 
 const userRoute=express.Router();
 
-userRoute.post("/register",validateMiddleware,registerController)
+userRoute.post("/register", validateMiddleware(userValidator),registerController)
 userRoute.post("/login", loginController)
+userRoute.get("/me", authMiddleware, myProfileController)
 
 module.exports = {userRoute}
