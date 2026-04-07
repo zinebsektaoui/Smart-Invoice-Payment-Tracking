5d92a279c10d6d53343f9210074e9042c7ef8fff
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-04-07 12:29:22 +0100

    limplementation de user routes

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
-const express = require("express")
-const dotenv = require("dotenv")
-const mongoose = require("mongoose")
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const { userRoute } = require("./Routes/authRoute");
 
-dotenv.config()
-const app = express()
-app.use(express.json())
+dotenv.config();
+const app = express();
+app.use(express.json());
 
+const port = process.env.PORT;
+const MONGODB_URL = process.env.MONGODB_URI;
 
-const port = process.env.PORT
-const MONGODB_URL = process.env.MONGODB_URI
+app.use("/api/auth", userRoute);
 
-app.listen(port, async() => {
-    console.log(`Server running on port ${port}`);
-    try{
-        let connexion = await mongoose.connect(MONGODB_URL)
-        console.log("Well connected to DB");
-    }catch(err){
-        console.log("Connexion failed : ", err.message);
-    }
-})
+app.listen(port, async () => {
+  console.log(`Server running on port ${port}`);
+  try {
+    let connexion = await mongoose.connect(MONGODB_URL);
+    console.log("Well connected to DB");
+  } catch (err) {
+    console.log("Connexion failed : ", err.message);
+  }
+});
