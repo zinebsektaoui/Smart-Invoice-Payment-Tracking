976d34dbbb08e28917c44e2b2fbe73af312007d4
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-04-07 12:26:55 +0100

    ajout de timestamps

--- Models/User.js
+++ Models/User.js
@@ -17,6 +17,10 @@ const userSchema = new mongoose.Schema({
         enum : ["Client", "admin"],
         default : "Client"
     }
+},{
+    timestamps : {
+        updatedAt : false
+   }
 })
 const User = mongoose.model("User", userSchema)
 module.exports = User
