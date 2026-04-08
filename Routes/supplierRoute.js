505fddb2c512a5e45d900492574e2a1a6d06b1e1
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-04-08 11:25:18 +0100

    l'implementation des route de suppresseion et de modification

--- Routes/supplierRoute.js
+++ Routes/supplierRoute.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const {createSupplier, getSuppliers, dropSupplier} = require("../Controllers/supplierController")
+const {createSupplier, getSuppliers, dropSupplier, getOneSupplier, update} = require("../Controllers/supplierController")
 const {authMiddleware} = require("../Middlewares/authMiddleware")
 const {roleMiddleware} = require("../Middlewares/roleMiddleware")
 const validateMiddleware = require("../Middlewares/validateMiddleware")
@@ -9,6 +9,8 @@ const supplierRoute = express.Router()
 
 supplierRoute.post("/", authMiddleware, roleMiddleware("Client"), validateMiddleware(supplierValidator), createSupplier)
 supplierRoute.get("/", authMiddleware, roleMiddleware("Client"), getSuppliers)
-supplierRoute.get("/:id", authMiddleware, roleMiddleware("Client"), dropSupplier)
+supplierRoute.delete("/:id", authMiddleware, roleMiddleware("Client"), dropSupplier)
+supplierRoute.get("/:id", authMiddleware, roleMiddleware("Client"), getOneSupplier)
+supplierRoute.put("/:id", authMiddleware, roleMiddleware("Client"), update)
 
 module.exports = supplierRoute
