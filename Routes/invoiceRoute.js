6fecfde6e42bd65200d31a406c6e4e05de90ee0b
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-04-10 16:05:25 +0100

    implementation des routes

--- Routes/invoiceRoute.js
+++ Routes/invoiceRoute.js
@@ -1,14 +1,16 @@
 const express = require("express")
-const {createInvoice, getAll, getById} = require("../Controllers/invoiceController")
+const {createInvoice, getAll, getById, deleteInvoice, update} = require("../Controllers/invoiceController")
 const {roleMiddleware} = require("../Middlewares/roleMiddleware")
 const validateMiddleware = require("../Middlewares/validateMiddleware")
 const {authMiddleware} = require("../Middlewares/authMiddleware")
 const invoiceValidator = require("../Validators/invoiceValidator")
-
+const {correspending} = require("../Middlewares/Correspondence")
 const invoiceRoute = express.Router()
 
 invoiceRoute.post("/", authMiddleware, roleMiddleware("Client"), validateMiddleware(invoiceValidator), createInvoice)
 invoiceRoute.get("/", authMiddleware, roleMiddleware("Client"), getAll)
-invoiceRoute.get("/:id", authMiddleware, roleMiddleware("Client"), getById)
+invoiceRoute.get("/:id", authMiddleware, roleMiddleware("Client"), correspending, getById)
+invoiceRoute.delete("/:id", authMiddleware, roleMiddleware("Client"), correspending, deleteInvoice)
+invoiceRoute.put("/:id", authMiddleware, roleMiddleware("Client"), correspending, update)
 
 module.exports = invoiceRoute
