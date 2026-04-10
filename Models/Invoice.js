ca2ab70b302c0412446f6ffe4ece0576456b717d
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-04-10 18:00:07 +0100

    update

--- Models/Invoice.js
+++ Models/Invoice.js
@@ -1,27 +1,45 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const invoiceSchema = new mongoose.Schema({
-    supplierId : {
-        type : String,
-        required : true
-    }, amount : {
-        type : Number,
-        required : true
-    }, dueDate : {
-        type : Date,
-        required : true        
-    }, description : {
-        type : String
-    },status : {
-        type : String,
-        enum : ["unpaid", "partially_paid", "paid"],
-        default : "unpaid"
-    },clientId : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "User"
-    }
-}, {
-    timestamps : true
-})
-const Invoice = mongoose.model("invoice", invoiceSchema)
-module.exports = Invoice
+const invoiceSchema = new mongoose.Schema(
+  {
+    amount: {
+      type: Number,
+      required: true,
+    },
+    dueDate: {
+      type: Date,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    status: {
+      type: String,
+
+      enum: ["unpaid", "partially_paid", "paid"],
+      default: "unpaid",
+    },
+    // tottalPaid: {
+    //     type: Number,
+    //     default: 0,
+    // },
+    // remainingAmount: {
+    //     type: Number,
+    //     default: 0
+    // },
+    supplierId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Supplier",
+      required: true,
+    },
+    clientId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+const Invoice = mongoose.model("invoice", invoiceSchema);
+module.exports = Invoice;
