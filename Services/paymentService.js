f47410ce352a67df35ba88ae004ac80e95435f4e
zineb_sektaoui <zinebsektaoui1@gmail.com>
2026-04-11 19:37:56 +0200

    payment caòlculation

--- Services/paymentService.js
+++ Services/paymentService.js
@@ -2,39 +2,78 @@ const Payment = require('../Models/Payment');
 const Invoice = require('../Models/Invoice');
 
 async function createPaymentService(req, res) {
-    const { invoiceId } = req.params;
-    const { amount, paymentDate, method, note, supplierId } = req.body;
-    const invoice = await Invoice.findById(invoiceId);
+  const { id } = req.params;
+  const { amount, paymentDate, method, note, supplierId } = req.body;
+
+  if (!amount || !paymentDate || !method) {
+    return res.status(400).json({ error: "Missing required fields" });
+  }
+
+  const invoice = await Invoice.findById(id);
+
+  if (!invoice) {
+    return res.status(404).json({ error: "Invoice not found" });
+  }
+
+  if (invoice.status === "paid") {
+    return res.status(422).json({ error: "Invoice is already paid" });
+  }
+  // update totals
+  invoice.tottalPaid += amount;
+
+  invoice.remainingAmount = invoice.amount - invoice.tottalPaid;
+  await invoice.save();
+  if (amount > invoice.remainingAmount) {
+    return res.status(422).json({
+      message: `You can't pay more than ${invoice.remainingAmount}`
+    });
+  }
+  // update status
+  if (invoice.tottalPaid === invoice.amount) {
+    invoice.status = "paid";
+  } else if (invoice.tottalPaid > 0) {
+    invoice.status = "partially_paid";
+  }
+
+  await invoice.save();
+
+  const payment = new Payment({
+    invoiceId: id,
+    userId: req.user.userId,
+    supplierId,
+    clientId : req.user.userId,
+    method,
+    amount,
+    paymentDate,
+    note,
+    invoiceDetails: invoice
+  });
+
+  await payment.save();
+
+  return res.status(201).json({
+    payment: payment,
+    invoice: invoice  // hadi tzid bash treturni l'invoice m3a l'paiement
+    });
+}
+
+async function getInvoiceDetailsService(req, res) {
+    const {id} = req.params
+    const invoice = await Invoice.findById(id);
     if (!invoice) {
         return res.status(404).json({ error: "Invoice not found" });
     }
-    if (invoice.status === "paid") {
-        return res.status(422).json({ error: "Invoice is already paid" });
-    }
-    if(amount > 0){//amount payed
-        invoice.status = "partially_paid"
-        await invoice.save();
-    }
-    if(amount >= invoice.amount){//amount payed
-        invoice.status = "paid";
-        await invoice.save();
-    }
-    if(!amount || !paymentDate || !method){
-        return res.status(400).json({ error: "Missing required fields" })
-    }
-
-    const payment = new Payment({
-        invoiceId: invoiceId,
-        userId: req.user.UserId,
-        supplierId,
-        method,
-        amount,
-        paymentDate,
-        note
-    })
-    await payment.save()
-    // console.log(invoiceId); err hena
-    return payment
+    const payments = await Payment.find({invoiceId: id}).select("method paymentDate amount").sort({ paymentDate: -1 })
+    return {
+        invoice: {
+            id: invoice._id,
+            amount: invoice.amount,
+            totalPaid: invoice.tottalPaid,
+            remainingAmount: invoice.amount - invoice.tottalPaid,
+            status: invoice.status
+        },
+        payments: payments
+    };
 }
 
-module.exports = {createPaymentService}
+module.exports = { createPaymentService, getInvoiceDetailsService };
