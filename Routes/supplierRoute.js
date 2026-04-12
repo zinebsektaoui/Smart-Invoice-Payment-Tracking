8b657c0d0717b7cc7e01db71b47d699ad659b861
zineb_sektaoui <zinebsektaoui1@gmail.com>
2026-04-12 19:15:26 +0200

    l'implementation des stats route

--- Routes/supplierRoute.js
+++ Routes/supplierRoute.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const {createSupplier, getSuppliers, dropSupplier, getOneSupplier, update} = require("../Controllers/supplierController")
+const {createSupplier, getSuppliers, dropSupplier, getOneSupplier, update, getStats} = require("../Controllers/supplierController")
 const {authMiddleware} = require("../Middlewares/authMiddleware")
 const {roleMiddleware} = require("../Middlewares/roleMiddleware")
 const validateMiddleware = require("../Middlewares/validateMiddleware")
@@ -12,5 +12,7 @@ supplierRoute.get("/", authMiddleware, roleMiddleware("Client"), getSuppliers)
 supplierRoute.delete("/:id", authMiddleware, roleMiddleware("Client"), dropSupplier)
 supplierRoute.get("/:id", authMiddleware, roleMiddleware("Client"), getOneSupplier)
 supplierRoute.put("/:id", authMiddleware, roleMiddleware("Client"), update)
+supplierRoute.get("/:id/stats", authMiddleware, roleMiddleware("Client"), getStats)
+
 
 module.exports = supplierRoute
